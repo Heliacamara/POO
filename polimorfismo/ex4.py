feaf198663b09c797e3e3f2e5df5888a1d7fe72d
Heliacamara <camara.helia@escolar.ifrn.edu.br>
2026-04-23 20:21:01 -0300

    20:20

---
+++ polimorfismo/ex4.py
@@ -0,0 +1,17 @@
+class Veiculo:
+    def mover(self):
+        return "Veiculo se movendo"
+    
+class Carro(Veiculo):
+    def mover(self):
+        return "Carro andando"
+    
+class Moto(Veiculo):
+    def mover(self):
+        return "Moto se locomovendo"
+    
+class Bicicleta(Veiculo):
+    def mover(self):
+        return "Bicicleta se movendo"
+
+
